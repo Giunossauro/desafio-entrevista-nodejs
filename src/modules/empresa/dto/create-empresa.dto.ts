fa1232990cd0d3a040cc3a46cdd58d6dc3105161
Giuliano Morelli <giuandroide@gmail.com>
2023-01-09 08:40:18 -0300

    refactor: create empresa dto

--- src/modules/empresa/dto/create-empresa.dto.ts
+++ src/modules/empresa/dto/create-empresa.dto.ts
@@ -1,1 +1,18 @@
-export class CreateEmpresaDto {}
+import { IsString } from "class-validator";
+
+export class CreateEmpresaDto {
+  @IsString()
+  readonly nome!: string;
+
+  @IsString()
+  readonly cnpj!: string;
+
+  @IsString()
+  readonly telefone!: string;
+
+  @IsString()
+  readonly qtdVagasCarros!: number;
+
+  @IsString()
+  readonly qtdVagasMotos!: number;
+}
