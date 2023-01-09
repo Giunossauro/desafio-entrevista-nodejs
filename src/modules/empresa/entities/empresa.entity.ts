20210e0e8982bd5d981da44754683ea32cfbf1fd
Giuliano Morelli <giuandroide@gmail.com>
2023-01-08 23:59:54 -0300

    feat(entity): create empresa and it's relations

---
+++ src/modules/empresa/entities/empresa.entity.ts
@@ -0,0 +1,66 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, JoinColumn } from "typeorm";
+import { Endereco } from "./related/endereco.entity";
+import { Login } from "./related/login.entity";
+import { Veiculo } from "./related/veiculo.entity";
+
+@Entity("treatments", { database: "estacionamento", schema: "empresa" })
+export class Empresa {
+  @PrimaryGeneratedColumn("uuid", { name: "empresa_id" })
+  empresaId!: string;
+
+  @Column({ name: "nome", type: "varchar", length: 150 })
+  nome!: string;
+
+  @Column({ name: "cnpj", type: "char", length: 14, unique: true })
+  cnpj!: string;
+
+  @Column({ name: "telefone", type: "varchar", length: 11 })
+  telefone!: string;
+
+  @Column({ name: "qtd_vagas_carros", type: "smallint", default: 0 })
+  qtdVagasCarros!: number;
+
+  @Column({ name: "qtd_vagas_motos", type: "smallint", default: 0 })
+  qtdVagasMotos!: number;
+
+  @OneToOne(
+    () => Login,
+    (login) => login.empresa,
+    { nullable: false, cascade: true }
+  )
+  @JoinColumn()
+  login!: Login;
+
+  @JoinTable({ name: "empresa_enderecos" })
+  @OneToMany(
+    () => Endereco,
+    (endereco) => endereco.empresa,
+    { cascade: true }
+  )
+  enderecos: Endereco[];
+
+  @JoinTable({ name: "empresa_veiculos" })
+  @OneToMany(
+    () => Veiculo,
+    (veiculo) => veiculo.empresa,
+    { cascade: true }
+  )
+  veiculos: Veiculo[];
+
+  init() {
+    this.empresaId = '';
+    this.nome = '';
+    this.cnpj = '';
+    this.qtdVagasCarros = 0;
+    this.qtdVagasMotos = 0;
+  }
+
+  @CreateDateColumn({ name: "created_at", type: "timestamp" })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: "updated_at", type: "timestamp" })
+  updatedAt!: Date;
+
+  @DeleteDateColumn({ name: "deleted_at", type: "timestamp" })
+  deletedAt?: Date;
+}
---
+++ src/modules/empresa/entities/related/veiculo.entity.ts
@@ -0,0 +1,46 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, ManyToOne } from "typeorm";
+import { Empresa } from "../../entities/empresa.entity";
+
+@Entity("veiculo", { database: "estacionamento", schema: "empresas" })
+export class Veiculo {
+  @PrimaryGeneratedColumn("uuid", { name: "veiculo_id" })
+  veiculoId!: string;
+
+  @ManyToOne(
+    () => Empresa,
+    (empresa) => empresa.veiculos
+  )
+  empresa: Empresa;
+
+  @Column({ name: "marca", type: "varchar", length: 20 })
+  marca!: string;
+
+  @Column({ name: "modelo", type: "varchar", length: 40 })
+  modelo!: string;
+
+  @Column({ name: "cor", type: "varchar", length: 25 })
+  cor?: string;
+
+  @Column({ name: "placa", type: "char", length: 7 })
+  placa!: string;
+
+  @Column({ name: "tipo", type: "char", length: 1 })
+  tipo!: string; // m = moto | c = carro
+
+  init() {
+    this.marca = '';
+    this.modelo = '';
+    this.cor = '';
+    this.placa = '';
+    this.tipo = '';
+  }
+
+  @CreateDateColumn({ name: "created_at", type: "timestamp" })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: "updated_at", type: "timestamp" })
+  updatedAt!: Date;
+
+  @DeleteDateColumn({ name: "deleted_at", type: "timestamp" })
+  deletedAt?: Date;
+}
